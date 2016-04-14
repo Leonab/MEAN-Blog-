a96319d0d5d777491832812def6fef17f0bb1f1a
Leonab <leonabrocks@yahoo.com>
2016-04-14 23:55:55 +0530

    1st commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('*', function(request, response) {
+    response.sendfile('./public/index.html');
+});
+
+module.exports = router;
